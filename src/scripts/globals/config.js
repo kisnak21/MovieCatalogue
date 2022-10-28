da3a0572c655475cb77990308da0907ecf0f1155
kisnak21 <krisnastya21@gmail.com>
2022-10-28 09:06:55 +0700

    implementation application shell and show movie from APi

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'ef0ff248560088eddcd55902789a861b',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
