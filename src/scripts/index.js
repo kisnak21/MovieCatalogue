151c1c2f4ac854a5fafc9fa75b51a0739892c855
kisnak21 <krisnastya21@gmail.com>
2022-11-02 16:38:14 +0700

    add webscoket to refresh movies

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,8 @@ import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 import App from './views/app';
 
 const app = new App({
@@ -17,4 +19,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
