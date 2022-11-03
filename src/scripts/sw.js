7112ff36fc4c1c634e7aa67f52752d7f167a479a
kisnak21 <krisnastya21@gmail.com>
2022-11-03 11:55:00 +0700

    finished: add subscription

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,32 +1,37 @@
-import 'regenerator-runtime';
-import CacheHelper from './utils/cache-helper';
+import { precacheAndRoute } from 'workbox-precaching';
 
-// Daftar asset yang akan dicaching
-const assetsToCache = [
-  './',
-  './icons/maskable_icon.png',
-  './icons/maskable_icon_x48.png',
-  './icons/maskable_icon_x72.png',
-  './icons/maskable_icon_x96.png',
-  './icons/maskable_icon_x128.png',
-  './icons/maskable_icon_x192.png',
-  './icons/maskable_icon_x384.png',
-  './icons/maskable_icon_x512.png',
-  './index.html',
-  './favicon.png',
-  './app.bundle.js',
-  './app.webmanifest',
-  './sw.bundle.js',
-];
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
+self.addEventListener('install', () => {
+  console.log('Service Worker: Installed');
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(CacheHelper.deleteOldCache());
+self.addEventListener('push', (event) => {
+  console.log('Service Worker: Pushed');
+
+  const dataJson = event.data.json();
+  const notification = {
+    title: dataJson.title,
+    options: {
+      body: dataJson.options.body,
+      icon: dataJson.options.icon,
+      image: dataJson.options.image,
+    },
+  };
+
+  event.waitUntil(self.registration.showNotification(notification.title, notification.options));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(CacheHelper.revalidateCache(event.request));
+self.addEventListener('notificationclick', (event) => {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+
+  const chainPromise = async () => {
+    console.log('Notification has been clicked');
+    await self.clients.openWindow('https://www.dicoding.com/');
+  };
+
+  event.waitUntil(chainPromise());
 });
